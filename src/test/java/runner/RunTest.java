39971b97103736302f078e0aa01987f4238029c7
Kirils Seve <kirils.seve@ask.fm>
2020-04-06 20:51:08 +0300

    allure-reports

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -5,8 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty"},
-        features = {"/Users/vulros/Documents/FinalProject/src/test/resources/features"},
+@CucumberOptions(plugin = {"pretty","io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm"},
+//@CucumberOptions(plugin = {"pretty",},
+ features = {"/Users/vulros/Documents/FinalProject/src/test/resources/features"},
         glue = {"steps", "hooks"},
         tags = {"@Shoptest"}
 )
